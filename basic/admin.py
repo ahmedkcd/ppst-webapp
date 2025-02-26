cf8241d4aa53480ddecd0e8383e725327d088a20
Matt Cossari <mattcossari@outlook.com>
2025-02-26 16:00:34 -0500

    Removed doctor model Instead of using an abstract user just use djangos built in user model

--- basic/admin.py
+++ basic/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Computed
+from .models import Doctor, TestSession, Stimuli, Response, Statistics, Computed
+
 
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(TestSession)
+admin.site.register(Stimuli)
+admin.site.register(Response)
+admin.site.register(Statistics)
 admin.site.register(Computed)
