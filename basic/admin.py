e82c33553a9c961f244bb12f57eb2aa5f41504cb
Matt Cossari <mattcossari@outlook.com>
2025-02-26 16:14:38 -0500

    Removed computed model

--- basic/admin.py
+++ basic/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from .models import Doctor, TestSession, Stimuli, Response, Statistics, Computed
+from .models import TestSession, Stimuli, Response, Statistics
 
 
 # Register your models here.
-admin.site.register(Doctor)
 admin.site.register(TestSession)
 admin.site.register(Stimuli)
 admin.site.register(Response)
 admin.site.register(Statistics)
-admin.site.register(Computed)
--- basic/models.py
+++ basic/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User  # Use built-in User model
 
-# Computed objects cache computations that were already performed
-class Computed(models.Model):
-    input = models.IntegerField()
-    output = models.IntegerField()
-    time_computed = models.DateTimeField(null=True)
-
-    def __str__(self):
-        return f"{self.input} -> {self.output}"
-
-
 # TestSession now references User instead of Doctor
 class TestSession(models.Model):
     test_id = models.AutoField(primary_key=True)
