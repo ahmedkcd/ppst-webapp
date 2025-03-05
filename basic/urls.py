79a44f03b6c9f638ee838a99cd63c5676a808159
Matt Cossari <mattcossari@outlook.com>
2025-03-05 12:29:02 -0500

    Basic test creating page

--- basic/urls.py
+++ basic/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
 
-app_name="basic"
+from .views import generate_test, test_page
+
+app_name = "basic"
 
 urlpatterns = [
-   # path("compute/<int:value>", views.compute, name="compute"),
-   # path("isprime/<int:value>", views.isprime, name="isprime"),
+    # path("compute/<int:value>", views.compute, name="compute"),
+    # path("isprime/<int:value>", views.isprime, name="isprime"),
+    path('generate-test/', generate_test, name='generate_test'),
+    path('test-page/', test_page, name='test_page'),
 ]
