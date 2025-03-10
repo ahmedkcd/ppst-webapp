4648bf4778e85a3f4d3a21239d6f1b67e6daa4e4
ahmedkcd <kaceda24@students.rowan.edu>
2025-03-10 14:20:27 -0400

    Added testpage_response url
    Added the url path

--- basic/urls.py
+++ basic/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
    # path("compute/<int:value>", views.compute, name="compute"),
    # path("isprime/<int:value>", views.isprime, name="isprime"),
    path("testpage/", views.testpage, name="testpage"),
+   path("testpage_response/", views.testpage_response, name="testpage_response"),
 ]
