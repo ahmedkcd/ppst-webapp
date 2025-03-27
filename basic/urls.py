6e23ba744d2d80cf4ff5715d8f15c236595c460a
Adam Mahrous <adamahro82@gmail.com>
2025-03-27 17:58:06 -0400

    fixed data fetching, working on download

--- basic/urls.py
+++ basic/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .views import export_test_data
 
 app_name="basic"
 
 urlpatterns = [
    # path("compute/<int:value>", views.compute, name="compute"),
    # path("isprime/<int:value>", views.isprime, name="isprime"),
-   path("testresults/", views.testresults, name="testresults")
+   path("testresults/", views.testresults, name="testresults"),
+   path("export_test_data/", views.export_test_data, name="export_test_data")
 ]
