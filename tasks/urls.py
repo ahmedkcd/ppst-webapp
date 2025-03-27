784f3eebbf1d8c34ef8941cbf5f28b256c8204e3
Matt Cossari <164905831+mcossari@users.noreply.github.com>
2025-03-27 18:59:13 -0400

    Delete tasks directory

--- tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name="tasks"
-
-urlpatterns = [
-    path("listnotifications/<str:username>/", views.listnotifications, name="listnotifications"),
-]
