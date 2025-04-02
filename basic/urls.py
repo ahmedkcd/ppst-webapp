09054347d2ef149c4afbfea21cea3039993d6af6
Govinda Avinesh Muthuselvam <goviadi03@gmail.com>
2025-04-02 12:14:21 -0400

    Ready to be pushed to main

--- basic/urls.py
+++ basic/urls.py
@@ -1,32 +1,23 @@
 from django.urls import path
+from . import views
 
-from .views import (
-    generate_test, test_page, take_test, 
-    get_responses, submit_response, user_login, 
-    dashboard, results, statistics, newtest, 
-    base, landing, logout_view
-)
-
-app_name = "basic"
+app_name="basic"
 
 urlpatterns = [
-    # path("compute/<int:value>", views.compute, name="compute"),
-    # path("isprime/<int:value>", views.isprime, name="isprime"),
+   # path("compute/<int:value>", views.compute, name="compute"),
+   # path("isprime/<int:value>", views.isprime, name="isprime"),
+    # path("login", views.login, name="login"),
+    path("login", views.user_login, name="login"),
+    path("dashboard", views.dashboard, name='dashboard'),
+    path("results", views.results, name='results'),
+    path("statistics", views.statistics, name='statistics'),
+    path("newtest", views.newtest,name="newtest"),
+    path("base",views.base,name="base"),
+    path("", views.landing, name='landing'),
+    path('logout', views.logout_view, name='logout'),
+    path('testpage', views.test_page, name='testpage'),
+    
+    
+    
 
-    # User-related paths
-    path("login", user_login, name="login"),
-    path("dashboard", dashboard, name='dashboard'),
-    path("results", results, name='results'),
-    path("statistics", statistics, name='statistics'),
-    path("newtest", newtest, name="newtest"),
-    path("base", base, name="base"),
-    path("", landing, name='landing'),
-    path('logout', logout_view, name='logout'),
-
-    # Test-related paths
-    path('generate-test/', generate_test, name='generate_test'),
-    path('test-page/', test_page, name='test_page'),
-    path('take-test/', take_test, name='take_test'),
-    path('get-responses/', get_responses, name='get_responses'),
-    path('submit-response/', submit_response, name='submit_responses'),
 ]
