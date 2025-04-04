4c21f6f791ddbba055bec5ea2ad37387584f58c0
Adam Mahrous <adamahro82@gmail.com>
2025-04-03 22:49:52 -0400

    Url fixes

--- basic/urls.py
+++ basic/urls.py
@@ -5,7 +5,7 @@ from .views import (
     test_intro, test_instructions, take_test, get_responses, submit_response, practice_test,
     get_practice_responses, practice_countdown, practice_transition, test_complete, doctor_dashboard,
     doctor_login_view,doctor_logout_view,doctor_newtest,doctor_results,doctor_statistics,doctor_test_page,
-    doctor_user_login, base, landing
+    doctor_user_login, base, landing, testresults, export_test_data
 )
 
 app_name = "basic"
@@ -37,7 +37,7 @@ urlpatterns = [
     path("", landing, name='landing'),
     path('doctor-logout', doctor_logout_view, name='doctor-logout'),
     path('doctor-testpage', doctor_test_page, name='doctor-testpage'),
-    path("testresults/", views.testresults, name="testresults"),
-    path("export_test_data/", views.export_test_data, name="export_test_data")
+    path("testresults/", testresults, name="testresults"),
+    path("export_test_data/", export_test_data, name="export_test_data")
 
 ]
