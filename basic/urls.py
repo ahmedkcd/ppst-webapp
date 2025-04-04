bbfabc89a116e76fbe98db1a03bc1e2b1de9b26c
Tanner Dodge <trixdodge@gmail.com>
2025-04-04 14:15:37 -0400

    Restore test_statistics path

--- basic/urls.py
+++ basic/urls.py
@@ -1,28 +1,16 @@
 from django.urls import path
 
-<<<<<<< HEAD
-from .views import generate_test, test_page, take_test, get_responses, submit_response, test_statistics
-=======
 from .views import (
     generate_test, test_page, login_view, dashboard, logout_view,
     test_intro, test_instructions, take_test, get_responses, submit_response, practice_test,
     get_practice_responses, practice_countdown, practice_transition, test_complete, doctor_dashboard,
     doctor_login_view,doctor_logout_view,doctor_newtest,doctor_results,doctor_statistics,doctor_test_page,
-    doctor_user_login, base, landing, testresults, export_test_data
+    doctor_user_login, base, landing, testresults, export_test_data, test_statistics
 )
->>>>>>> main
 
 app_name = "basic"
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("test_statistics/<int:test_id>/", test_statistics, name="test_statistics"),
-    path('generate-test/', generate_test, name='generate_test'),
-    path('test-page/', test_page, name='test_page'),
-    path('take-test/', take_test, name='take_test'),
-    path('get-responses/', get_responses, name='get_responses'),
-    path('submit-response/', submit_response, name='submit_responses'),
-=======
     path("login/", login_view, name="login"),
     path("dashboard/", dashboard, name="dashboard"),
     path("logout/", logout_view, name="logout"),
@@ -50,7 +38,7 @@ urlpatterns = [
     path('doctor-logout', doctor_logout_view, name='doctor-logout'),
     path('doctor-testpage', doctor_test_page, name='doctor-testpage'),
     path("testresults/", testresults, name="testresults"),
-    path("export_test_data/", export_test_data, name="export_test_data")
+    path("export_test_data/", export_test_data, name="export_test_data"),
+    path("test_statistics/<int:test_id>/", test_statistics, name="test_statistics"),
 
->>>>>>> main
 ]
