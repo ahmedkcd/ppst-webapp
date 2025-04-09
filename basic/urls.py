1e940a6ff4b247928bebfdf7081562a0347ab4f3
Amyr Smith <smitha49@students.rowan.edu>
2025-04-09 18:04:20 -0400

    It is running smoothly

--- basic/urls.py
+++ basic/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
 from .views import (
-    generate_test, test_page, login_view, dashboard, logout_view,
-    test_intro, test_instructions, take_test, get_responses, submit_response, practice_test,
-    get_practice_responses, practice_countdown, practice_transition, test_complete, doctor_dashboard,
-    doctor_login_view,doctor_logout_view,doctor_newtest,doctor_results,doctor_statistics,doctor_test_page,
+    generate_test, test_page, login_view, dashboard, logout_view, test_intro, test_instructions, take_test, 
+    get_responses, submit_response, practice_test, get_practice_responses, practice_countdown, practice_transition, 
+    test_complete, doctor_dashboard, doctor_logout_view, doctor_newtest, doctor_results, doctor_statistics, doctor_test_page, 
     doctor_user_login, base, landing, testresults, export_test_data, test_statistics
 )
 
@@ -28,15 +27,15 @@ urlpatterns = [
     path("practice/transition/", practice_transition, name="practice-transition"),
     path("test/complete/", test_complete, name="test_complete"),
 
-    path("doctor-login", doctor_user_login, name="doctor-login"),
-    path("doctor-dashboard", doctor_dashboard, name='doctor-dashboard'),
-    path("doctor-results", doctor_results, name='doctor-results'),
-    path("doctor-statistics", doctor_statistics, name='doctor-statistics'),
-    path("doctor-newtest", doctor_newtest,name="doctor-newtest"),
-    path("base",base,name="base"),
+    path("doctor-login/", doctor_user_login, name="doctor-login"),
+    path("doctor-dashboard/", doctor_dashboard, name='doctor-dashboard'),
+    path("doctor-results/", doctor_results, name='doctor-results'),
+    path("doctor-statistics/", doctor_statistics, name='doctor-statistics'),
+    path("doctor-newtest/", doctor_newtest,name="doctor-newtest"),
+    path("base/",base,name="base"),
     path("", landing, name='landing'),
-    path('doctor-logout', doctor_logout_view, name='doctor-logout'),
-    path('doctor-testpage', doctor_test_page, name='doctor-testpage'),
+    path("doctor-logout/", doctor_logout_view, name='doctor-logout'),
+    path("doctor-testpage/", doctor_test_page, name='doctor-testpage'),
     path("testresults/", testresults, name="testresults"),
     path("export_test_data/", export_test_data, name="export_test_data"),
     path("test_statistics/<int:test_id>/", test_statistics, name="test_statistics"),
