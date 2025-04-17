40b0bd899a4653d386babbb9930a3e378fb1c3cc
mcossari <164905831+mcossari@users.noreply.github.com>
2025-04-16 21:21:06 -0400

    Removed old urls

--- basic/urls.py
+++ basic/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from .views import (
-    generate_test, test_page, login_view, dashboard, logout_view,
+    generate_test, login_view, dashboard, logout_view,
     test_intro, test_instructions, take_test, get_responses, submit_response, practice_test,
     get_practice_responses, practice_countdown, practice_transition, test_complete, doctor_dashboard,submit_all_responses,
-    doctor_logout_view, doctor_newtest, doctor_results, doctor_statistics, doctor_test_page,
+    doctor_logout_view, doctor_newtest, doctor_results, doctor_statistics,
     doctor_user_login, base, landing, testresults, export_test_data, test_statistics
 )
 
@@ -17,7 +17,6 @@ urlpatterns = [
     path("test/intro/", test_intro, name="test_intro"),
     path("test/instructions/", test_instructions, name="test_instructions"),
     path("generate-test/", generate_test, name="generate_test"),
-    path("test-page/", test_page, name="test_page"),
     path("take-test/", take_test, name="take_test"),
     path("get-responses/", get_responses, name="get_responses"),
     path("submit-response/", submit_response, name="submit_responses"),
@@ -37,7 +36,6 @@ urlpatterns = [
     path("base", base, name="base"),
     path("", landing, name='landing'),
     path('doctor-logout', doctor_logout_view, name='doctor-logout'),
-    path('doctor-testpage', doctor_test_page, name='doctor-testpage'),
     path("testresults/", testresults, name="testresults"),
     path("export_test_data/", export_test_data, name="export_test_data"),
     path("test_statistics/<str:test_id>/", test_statistics, name="test_statistics")
